b7646315679d5226109d3100ab3126265d6763f3
Nathan Beck <nathan.beck@fleishman.com>
2017-10-09 16:48:13 -0500

    Build out basic layout and data structures

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,98 @@
 import React, { Component } from 'react';
 import './App.css';
+import client from '../Data/client';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      groups: client.buildState()
+    }
+  }
+
   render() {
+
+    const groups = this.state.groups.map(group => {
+      return (
+        <Group
+          key={group.title}
+          title={group.title}
+          year={group.year}
+          members={group.members}
+        />
+      );
+    });
+
     return (
       <div className="App">
-        Hello!
+        <Header />
+        <Search />
+        {groups}
       </div>
+    );
+  }
+}
+
+const Header = (props) => {
+  return (
+    <header>
+      <h1>Terrace House Social</h1>
+      <p>Click on a Terrace House commentator or cast member below to see their social media and other online profiles. Have an update to the list? Feel free to reach out. Please note: we are in no way affiliated with the show!</p>
+    </header>
+  );
+};
+
+class Search extends Component {
+  render() {
+    return (
+      <form>
+        Search goes here
+      </form>
+    );
+  }
+}
+
+class Group extends Component {
+  render() {
+    return (
+      <div className="Group">
+        <span>{this.props.year}</span>
+        <h2>{this.props.title}</h2>
+        <Members members={this.props.members} />
+      </div>
+    );
+  }
+}
+
+class Members extends Component {
+  render() {
+    const members = this.props.members.map(member => {
+      return (
+        <Member
+          key={member.id}
+          id={member.id}
+          name={member.name}
+          image={member.image}
+          accounts={member.accounts}
+        />
+      );
+    });
+    
+    return (
+      <ul className="Members">
+        {members}
+      </ul>
+    );
+  }
+}
+
+class Member extends Component {
+  render() {
+    return (
+      <li className="Member">
+        I am a member.
+      </li>
     );
   }
 }
---
+++ src/Data/client.js
@@ -0,0 +1,54 @@
+import json from './db';
+
+const client = {
+  getAllCommentators() {
+    return json.members.filter(member => member.type === 'commentator');
+  },
+
+  getAllCast() {
+    return json.members.filter(member => member.type === 'cast');
+  },
+
+  getMemberById(id) {
+    return json.members.find(member => member.id === id);
+  },
+
+  getMemberByName(name) {
+    return json.members.find(member => member.name === name);
+  },
+
+  getAllSeasons() {
+    const cast = this.getAllCast();
+    const seasonsAsStrings = new Set(cast.map(member => JSON.stringify(member.season)));
+    return Array.from(seasonsAsStrings).map(seasonString => JSON.parse(seasonString));
+  },
+
+  getMembersBySeasonName(seasonName) {
+    return json.members
+      .filter(member => member.type === 'cast')
+      .filter(cast => cast.season.name === seasonName);
+  },
+
+  buildState() {
+    
+    // Build collaborators object
+    const collaborators = {
+      title: "Collaborators",
+      members: this.getAllCommentators()
+    };
+
+    // Build array of seasons objects
+    const seasons = this.getAllSeasons().map(season => {
+      return {
+        title: season.name,
+        year: season.year,
+        members: this.getMembersBySeasonName(season.name)
+      }
+    });
+
+    return [collaborators, ...seasons];
+
+  }
+};
+
+export default client;
