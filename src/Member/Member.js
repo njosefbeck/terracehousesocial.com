4ce2be37870318823ce9605c0f734a0bbcb7abd0
Nathan Beck <nathan.beck@fleishman.com>
2017-10-12 15:19:18 -0500

    Add cast member images

--- src/Member/Member.js
+++ src/Member/Member.js
@@ -4,11 +4,24 @@ import './Member.css';
 import Icons from '../Icons/Icons';
 
 class Member extends Component {
+
+  getImage = (imageFileName) => {
+    let image = "";
+    
+    if (!imageFileName) {
+      image = "https://picsum.photos/200/200?image=823";
+    } else {
+      image = `${process.env.PUBLIC_URL}/images/members/${imageFileName}`;
+    }
+
+    return image;
+  }
+
   render() {
     return (
       <li className="Member">
         <div className="member-image-container">
-          <img className="member-image" src={this.props.image} alt={this.props.name} />
+          <img className="member-image" src={this.getImage(this.props.image)} alt={this.props.name} />
         </div>
         <div className="member-information-container">
           <h3 className="member-name-english">{this.props.name.english}</h3>
