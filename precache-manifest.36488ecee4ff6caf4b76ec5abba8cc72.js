9c7509506a370ff1906144d920844e9527591326
Nathan Beck <njosefbeck@gmail.com>
2019-05-24 07:34:36 -0500

    Updates

---
+++ precache-manifest.36488ecee4ff6caf4b76ec5abba8cc72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3910f13b9821f634eaeed9980ac607eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "721d05fe884a21636be3",
+    "url": "/static/css/main.d7100ea4.chunk.css"
+  },
+  {
+    "revision": "084b4c39a0aa0cca8699",
+    "url": "/static/js/2.8f5220a1.chunk.js"
+  },
+  {
+    "revision": "721d05fe884a21636be3",
+    "url": "/static/js/main.f99312b5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
