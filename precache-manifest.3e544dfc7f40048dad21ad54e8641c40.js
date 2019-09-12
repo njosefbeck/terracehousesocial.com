34fb693c012de650ea4895d15d0ab60485a59065
Nathan Beck <nathan.beck@monsanto.com>
2019-09-12 08:42:57 -0500

    Updates

---
+++ precache-manifest.3e544dfc7f40048dad21ad54e8641c40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a86850962460f517fe799dea31f32fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4cfe29b70e4d9a5ff1d",
+    "url": "/static/css/main.d7100ea4.chunk.css"
+  },
+  {
+    "revision": "2e39f6e74568e7c58ebb",
+    "url": "/static/js/2.d31689af.chunk.js"
+  },
+  {
+    "revision": "d4cfe29b70e4d9a5ff1d",
+    "url": "/static/js/main.7cf0fdfd.chunk.js"
+  },
+  {
+    "revision": "3563e76218f8dd581fe4",
+    "url": "/static/js/runtime~main.36cdb542.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a740cc7e211dfe02d58ec1567c2bb3c8.js"
+  "/precache-manifest.3e544dfc7f40048dad21ad54e8641c40.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
